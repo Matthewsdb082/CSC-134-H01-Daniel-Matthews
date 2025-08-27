57fdb12988541d1b9f40db4e0b941071d216a8a3
Daniel Matthews <matthewsdb082@mail.sandhills.edu>
2025-08-27 15:38:18 -0400

    divide by 0

---
+++ divideby0lab1.cpp
@@ -0,0 +1,20 @@
+//Daniel matthews
+//8/27/2025
+// divide by 0 lab
+
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main()
+int main()
+{
+    int num1 =10;
+    int num2 =0;
+    int product = num1 / num2;
+//this prints out the product of the two number from above
+    std::cout << product << std::endl;
+
+    return 0;
+}
