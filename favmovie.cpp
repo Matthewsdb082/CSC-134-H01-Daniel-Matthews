c50ce3838ea79fa174f8634e9cf416d824d6c041
Daniel Matthews <matthewsdb082@mail.sandhills.edu>
2025-08-29 22:55:43 -0400

    fm

--- favmovie.cpp
+++ favmovie.cpp
@@ -1,11 +1,13 @@
-
+//daniel matthews
+//8/29/2025
+// print my favorite movie and character
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "hello"<< endl;
-    cout << "ss"<< endl;
+    cout << "Favorite Movie: Quigley Down Under"<< endl;
+    cout << "Favorite Character: Matthew Quigley" << endl;
     return 0;
 }
