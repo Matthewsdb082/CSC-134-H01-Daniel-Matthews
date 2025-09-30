535c8957d28a36e4b731c8d1bfc8db3c81dac74f
Daniel Matthews <matthewsdb082@mail.sandhills.edu>
2025-09-29 21:44:52 -0400

    qq

---
+++ 929nestedloopmulttable.cpp
@@ -0,0 +1,24 @@
+//daniel matthews
+//9/29/25
+// multiple multiplcation table
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+int a =0;
+int b=0;
+    for( a=1;a<=10;a++)
+    {
+        
+        for ( b=1;b<=10;b++)
+        {
+         cout<<a*b<<"\t";
+        }
+        cout<<endl;
+    }
+
+
+return 0;
+}
