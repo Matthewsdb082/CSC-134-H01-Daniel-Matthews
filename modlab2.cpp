00e633b8a32282703746c714156e6d610baab4f1
Daniel Matthews <matthewsdb082@mail.sandhills.edu>
2025-10-23 09:45:00 -0400

    21

--- modlab2.cpp
+++ modlab2.cpp
@@ -9,12 +9,12 @@ using namespace std;
 int main()
 {
 ofstream outFile;
- outFile.open("week9_lab1.txt");
+ outFile.open("week9_lab2.txt");
  
 int input=0;
 int count=0;
 int sales =0;
-
+int total =0;
 
 cout<<"input number of days"<<endl;
 cin>>input;
@@ -25,8 +25,12 @@ cout<<"day "<<count<<" sales"<<endl;
 cin>>sales;
 
 outFile<<sales<<"\n";
+total = total + sales;
 }
+
   //close
  outFile.close();
  return 0;
 }
+
+
