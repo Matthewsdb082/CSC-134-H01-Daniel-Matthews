f303bb7c9e8235346f0c1022a90c410b83cbf494
Daniel Matthews <matthewsdb082@mail.sandhills.edu>
2025-10-26 18:47:56 -0400

    n

--- daytodaysale.cpp
+++ daytodaysale.cpp
@@ -1,5 +1,5 @@
 //daniel matthews
-//10/8/25
+//10/26/25
 //day to day sales txt
 #include <iostream> 
 #include <fstream>
@@ -8,22 +8,23 @@ using namespace std;
 //define the main()
 int main()
 {
+//open/creat file 
 ofstream outFile;
  outFile.open("week9_lab1.txt");
- 
+//variables
 int input=0;
 int count=0;
 int sales =0;
 
-
+//prompt/input
 cout<<"input number of days"<<endl;
 cin>>input;
-
+//loop / inputs
 for(count=1;count<=input;count++)
 {
 cout<<"day "<<count<<" sales"<<endl;
 cin>>sales;
-
+// prints to file
 outFile<<sales<<"\n";
 }
   //close
--- modlab2.cpp
+++ modlab2.cpp
@@ -1,5 +1,5 @@
 //daniel matthews
-//10/8/25
+//10/26/25
 //day to day sales txt
 #include <iostream> 
 #include <fstream>
@@ -8,26 +8,31 @@ using namespace std;
 //define the main()
 int main()
 {
+//open/create file
 ofstream outFile;
  outFile.open("week9_lab2.txt");
- 
+ //variables
 int input=0;
 int count=0;
 int sales =0;
 int total =0;
-
+// prompt/inputs
 cout<<"input number of days"<<endl;
 cin>>input;
-
+//loop/input
 for(count=1;count<=input;count++)
 {
 cout<<"day "<<count<<" sales"<<endl;
 cin>>sales;
-
+// print to file plus runing total cals
 outFile<<sales<<"\n";
 total = total + sales;
 }
-
+//output total to file
+outFile<<"\n";
+outFile<<"====================\n";
+outFile<<"Total sales: $"<<total<<"\n";
+outFile<<"====================\n";
   //close
  outFile.close();
  return 0;
